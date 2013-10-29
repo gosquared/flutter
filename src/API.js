455892f759c4517035ecca7041c44e668e89f65d
Simon Tabor <me@simontabor.com>
2013-10-29 13:15:11 +0000

    this appears to work

---
+++ src/API.js
@@ -0,0 +1,142 @@
+var qs = require('querystring');
+
+var FlutterAPI = module.exports = function(flutter) {
+  var self = this;
+
+  self.flutter = flutter;
+  self.debug = function(s) {
+    flutter.debug('API:' + s);
+  };
+  self.oauth = flutter.oauth;
+  self.opts = flutter.opts;
+  self.cache = flutter.cache;
+};
+
+FlutterAPI.prototype.key = function(token, k) {
+  return self.opts.prefix + token + '.' + k;
+};
+
+FlutterAPI.prototype.get = function(url, token, secret, cb) {
+  var self = this;
+
+  self.oauth.get(url, token, secret, function(err, data, res) {
+
+    if (err) {
+      self.debug('error getting data');
+      return cb(err);
+    }
+
+    if (res && res.headers) {
+      self.debug('checking limit header');
+      var limit = res.headers['x-rate-limit-remaining'];
+
+      if (limit === "0") {
+        self.debug('rate limit reached');
+        cb({limitReached: true, auth: data});
+        return;
+      }
+    }
+
+    var d;
+    try {
+      d = JSON.parse(data);
+    } catch(e) {};
+
+    if (!d) {
+      self.debug('invalid json');
+      return cb('invalid json', data);
+    }
+
+    if (d.error) {
+      self.debug('error found in response');
+      return cb(d);
+    }
+
+    if (self.opts.cache) {
+      self.cache.set(self.key(token, url), data);
+      self.cache.pexpire(self.key(token, url), self.opts.cache);
+    }
+
+    cb(null, d, res);
+  });
+}
+
+FlutterAPI.prototype.fetch = function(url, params, token, secret, cb) {
+  var self = this;
+
+  var p = '';
+  if (params && Object.keys(params).length) p = '?' + qs.stringify(params);
+
+  url = 'https://api.twitter.com/1.1/' + url + p;
+
+  self.debug('fetching');
+
+  if (!self.opts.cache) {
+    self.debug('cache disabled');
+    return self.get.apply(self, arguments);
+  }
+
+  self.debug('trying cache');
+
+  self.cache.get(self.key(token, url), function(err, res) {
+    if (err || !res) {
+      self.debug('not cached');
+      return self.get.apply(self, arguments);
+    }
+
+    var data;
+    try {
+      data = JSON.parse(res)
+    } catch(e) {
+      self.debug('invalid redis cache response');
+    }
+
+    if (!data) {
+      return self.get.apply(self, arguments);
+    }
+
+    self.debug('successfully fetched cached');
+    cb(null, data);
+
+  });
+
+};
+
+
+/* Utility wrappers around some basic Twitter API functions */
+/* They don't currently do any response formatting */
+
+FlutterAPI.prototype.verify = function(token, secret, cb) {
+  var self = this;
+
+  self.debug('verifying credentials');
+
+  self.fetch('account/verify_credentials.json', token, secret, cb);
+};
+
+FlutterAPI.prototype.retweets = function(token, secret, params, cb) {
+  var self = this;
+
+  self.debug('getting retweets');
+
+  self.fetch('statuses/mentions_timeline.json', params, token, secret, cb);
+
+};
+
+FlutterAPI.prototype.user = function(token, secret, params, cb) {
+  var self = this;
+
+  self.debug('getting user details');
+
+  self.fetch('users/lookup.json', params, token, secret, cb);
+};
+
+FlutterAPI.prototype.search = function(token, secret, params, cb) {
+  var self = this;
+
+  self.debug('searching');
+
+  self.fetch('search/tweets.json', params, token, secret, cb);
+};
+
+
---
+++ src/Flutter.js
@@ -0,0 +1,167 @@
+var OAuth = require('oauth').OAuth;
+var debug = require('debug')('Flutter');
+var redis = require('redis');
+
+var FlutterAPI = require('./API');
+
+var Flutter = module.exports = function(opts) {
+  var self = this;
+
+  self.opts = {
+
+    // allow logger overrides
+    debug: debug,
+
+    // twitter consumer key
+    consumerKey: null,
+
+    // twitter consumer secret
+    consumerSecret: null,
+
+    // the URL to redirect to after twitter authorisation
+    loginCallback: null,
+
+    // the URL to redirect to after authorisation is complete and we have tokens
+    // will not be used if authCallback is overridden
+    completeCallback: null,
+
+    // the connection callback to be called after a successful connect (required for error handling)
+    connectCallback: function(req, res) {
+      self.debug('connect callback');
+
+      if (req.error) {
+        self.debug('sending error getting request token');
+        return res.send('Error getting oAuth Request Token');
+      }
+    },
+
+    authCallback: function(req, res) {
+      self.debug('auth callback');
+
+      if (req.error) {
+        self.debug('sending error getting access token');
+        return res.send('Error getting oAuth Access Token');
+      }
+
+      res.redirect(self.opts.completeCallback);
+    },
+
+    // redis client options
+    redis: {
+      host: 'localhost',
+      port: 6379,
+      database: 0
+    },
+
+    // set to a redis client to use it for caching instead of creating a new connection
+    cacheClient: null,
+
+    // cache duration. set to a falsy value to disable caching
+    cache: 60000,
+
+    // prefix to use on cache keys
+    prefix: 'flutter:'
+  }
+
+  // option overrides
+  for (var i in opts) {
+
+    // allow for 1st level nesting nice overrides
+    if (typeof opts[i] === 'object') {
+      if (typeof self.opts[i] !== 'object') self.opts[i] = opts[i];
+      for (var j in opts[i]) {
+        self.opts[i][j] = opts[i][j];
+      }
+      continue;
+    }
+
+    self.opts[i] = opts[i]
+  }
+
+  self.debug = self.opts.debug;
+  self.debug('init');
+
+
+  self.oauth = new OAuth(
+    "https://twitter.com/oauth/request_token",
+    "https://twitter.com/oauth/access_token",
+    self.opts.consumerKey,
+    self.opts.consumerSecret,
+    "1.0A",
+    self.opts.loginCallback,
+    "HMAC-SHA1"
+  );
+
+  self.cache = self.opts.cacheClient;
+
+  if (!self.cache) {
+    self.debug('creating redis client');
+    self.cache = redis.createClient(self.opts.redis.port, self.opts.redis.host);
+    self.cache.select(self.opts.redis.database);
+  }
+
+
+  self.API = new FlutterAPI(self);
+};
+
+Flutter.prototype.connect = function(req, res, next) {
+  var self = this;
+
+  self.debug('getting request tokens');
+
+  self.oauth.getOAuthRequestToken(function(err, token, secret){
+    if (err) {
+      self.debug('error getting request tokens');
+      req.error = err;
+      return self.opts.connectCallback(req, res, next);
+    }
+
+    self.debug('got oauth request tokens');
+
+    req.session.oauthRequestToken = token;
+    req.session.oauthRequestTokenSecret = secret;
+
+    self.opts.connectCallback(req, res, next);
+
+    // redirect the user to the authorise page
+    res.redirect("https://twitter.com/oauth/authorize?oauth_token=" + token);
+
+  });
+};
+
+Flutter.prototype.auth = function(req, res, next) {
+  var self = this;
+
+  self.debug('getting access tokens')
+
+  self.oauth.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(err, accessToken, accessTokenSecret, results) {
+
+    if (err) {
+      self.debug('error getting access tokens');
+      req.error = err;
+      return self.opts.authCallback(req, res, next);
+    }
+
+    req.session.oauthAccessToken = accessToken;
+    req.session.oauthAccessTokenSecret = accessTokenSecret;
+
+    req.results = results;
+
+    self.opts.authCallback(req, res, next);
+  });
+};
+
+Flutter.prototype.logout = function(req, res) {
+  var self = this;
+
+  req.session.destroy(function(err) {
+    if (err) {
+      self.debug('error logging out');
+      return res.json({ error: 'error logging out' });
+    }
+
+    res.json({ logout: true });
+  });
+}
+
+
