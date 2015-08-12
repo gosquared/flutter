68d5902a0693b307ee3e87ae5f8c175c953777ee
Simon Tabor <me@simontabor.com>
2015-08-12 10:26:43 +0100

    v0.0.4 handle null opts, allow no redis conn, fixes #1

--- src/Flutter.js
+++ src/Flutter.js
@@ -67,8 +67,8 @@ var Flutter = module.exports = function(opts) {
   for (var i in opts) {
 
     // allow for 1st level nesting nice overrides
-    if (typeof opts[i] === 'object') {
-      if (typeof self.opts[i] !== 'object') self.opts[i] = opts[i];
+    if (opts[i] && typeof opts[i] === 'object') {
+      if (!self.opts[i] || typeof self.opts[i] !== 'object') self.opts[i] = opts[i];
       for (var j in opts[i]) {
         self.opts[i][j] = opts[i][j];
       }
@@ -94,7 +94,7 @@ var Flutter = module.exports = function(opts) {
 
   self.cache = self.opts.cacheClient;
 
-  if (!self.cache) {
+  if (!self.cache && self.opts.cache) {
     self.debug('creating redis client');
     self.cache = redis.createClient(self.opts.redis.port, self.opts.redis.host);
     self.cache.select(self.opts.redis.database);
@@ -128,7 +128,7 @@ Flutter.prototype.connect = function(req, res, next) {
 
     self.opts.connectCallback(req, res, next);
 
-    // sign in the user if the user has previously connected, else ask for authorization. 
+    // sign in the user if the user has previously connected, else ask for authorization.
     // see https://dev.twitter.com/oauth/reference/get/oauth/authenticate
     res.redirect("https://twitter.com/oauth/authenticate?oauth_token=" + token);
 
